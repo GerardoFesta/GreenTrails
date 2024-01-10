c525b500cb7596c280f7744fa07ed62fc072e119
Ernesto <ernesto7.edi@gmail.com>
2024-01-10 23:29:40 +0100

    fix(frontend): modified signup

--- frontend/src/app/componenti/signup/signup.component.ts
+++ frontend/src/app/componenti/signup/signup.component.ts
@@ -1,31 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
 export class SignupComponent implements OnInit {
-
+  togglePasswordVisibility() {
+  throw new Error('Method not implemented.');
+  }
+    emailValue: string = ''; // Valore dell'email inserito dall'utente
+    isEmailValid: boolean = true; // Flag per controllare la validità dell'email
   hide: boolean = true;
-  emailValue: string = ''; // Valore dell'email inserito dall'utente
-  isEmailValid: boolean = true; // Flag per controllare la validità dell'email
-
-  validateEmail() {
-    // Logica per la validazione dell'email (es. regex, verifica della struttura dell'email)
-    // In questo esempio, utilizziamo una semplice validazione per scopi dimostrativi
-    this.isEmailValid = /\S+@\S+\.\S+/.test(this.emailValue);
-
-  }
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
   
-  onSubmit(form: any){
-    console.log(form)
+    validateEmail() {
+      // Logica per la validazione dell'email (es. regex, verifica della struttura dell'email)
+      // In questo esempio, utilizziamo una semplice validazione per scopi dimostrativi
+      this.isEmailValid = /\S+@\S+\.\S+/.test(this.emailValue);
+  
+    }
+  
+    constructor() { }
+  
+    ngOnInit(): void {
+  
+    }
+  
+    onSubmit(form: any){
+      console.log(form)
+    }
+  
   }
-
-}
+  
+  
+  
