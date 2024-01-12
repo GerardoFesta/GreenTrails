422f7409c49ff1fbc7718b0d311a32b7cb9196b0
esetaro2 <bbot3333@gmail.com>
2024-01-12 11:37:54 +0100

    feat(frontend): deleted "singup" component from componenti folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,17 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ComponentiComponent } from './componenti/componenti.component';
-import { SingupComponent } from './componenti/singup/singup.component';
-import { DashboardComponent } from './componenti/dashboard./dashboard..component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentiComponent,
-    SingupComponent,
-    DashboardComponent
   ],
   imports: [
     BrowserModule,
