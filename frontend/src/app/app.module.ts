f865d926eafb30ce5b71a62b099856d6c0ec47fe
Ernesto <ernesto7.edi@gmail.com>
2024-01-13 10:49:06 +0100

    feat(frontend): add registrazione feature

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,21 +4,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ComponentiComponent } from './componenti/componenti.component';
-import { SingupComponent } from './componenti/singup/singup.component';
-import { DashboardComponent } from './componenti/dashboard./dashboard..component';
+import { RegistrazioneComponent } from './componenti/registrazione/registrazione.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentiComponent,
-    SingupComponent,
-    DashboardComponent
+    RegistrazioneComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule, ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
+    NgbModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
