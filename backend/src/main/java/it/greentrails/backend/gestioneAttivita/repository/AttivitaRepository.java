30b6d4b61c6fe02ffc373e4398dee0160c100a3d
Sys <28715512+sys-001@users.noreply.github.com>
2024-01-16 19:00:35 +0100

    feat(backend): add search by query and categories (service)

--- backend/src/main/java/it/greentrails/backend/gestioneAttivita/repository/AttivitaRepository.java
+++ backend/src/main/java/it/greentrails/backend/gestioneAttivita/repository/AttivitaRepository.java
@@ -1,6 +1,7 @@
 package it.greentrails.backend.gestioneAttivita.repository;
 
 import it.greentrails.backend.entities.Attivita;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,11 @@ public interface AttivitaRepository extends JpaRepository<Attivita, Long> {
 
   @Query("SELECT a FROM Attivita a WHERE a.gestore.id = ?1")
   Page<Attivita> findByGestore(Long idGestore, Pageable pageable);
+
+  @Query("SELECT a FROM Attivita a WHERE a.nome ILIKE %?1% OR a.citta ILIKE %?1%")
+  List<Attivita> findByQuery(String query);
+
+  @Query("SELECT a FROM Attivita a JOIN Categoria c WHERE c.id = ?1")
+  List<Attivita> findByCategoria(long idCategoria);
+
 }
---
+++ backend/src/main/java/it/greentrails/backend/gestioneRicerca/service/RicercaServiceImpl.java
@@ -0,0 +1,54 @@
+package it.greentrails.backend.gestioneRicerca.service;
+
+import it.greentrails.backend.entities.Attivita;
+import it.greentrails.backend.entities.Categoria;
+import it.greentrails.backend.gestioneAttivita.repository.AttivitaRepository;
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.geo.Point;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RicercaServiceImpl implements RicercaService {
+
+  private final AttivitaRepository repository;
+
+
+  @Override
+  public List<Attivita> findAttivita(String query) throws InvalidParameterException {
+    if (query == null || query.isBlank()) {
+      throw new InvalidParameterException("La query è vuota.");
+    }
+    return repository.findByQuery(query);
+  }
+
+  @Override
+  public List<Attivita> findAttivitaByCategorie(List<Categoria> categorie)
+      throws InvalidParameterException {
+    if (categorie == null || categorie.isEmpty()) {
+      throw new InvalidParameterException("La lista delle categorie è vuota");
+    }
+    return categorie.stream().map(c -> repository.findByCategoria(c.getId())).reduce(
+        (l1, l2) -> l1.stream()
+            .filter(l2::contains)
+            .collect(Collectors.toList())
+    ).orElse(new ArrayList<>());
+  }
+
+  @Override
+  public List<Attivita> findAttivitaByPosizione(Point coordinate, double raggio)
+      throws InvalidParameterException {
+    if (coordinate == null) {
+      throw new InvalidParameterException("Le coordinate sono vuote.");
+    }
+    if (raggio < 0) {
+      throw new InvalidParameterException("Il raggio non è valido.");
+    }
+    // TODO: implementare ricerca per coordinate e raggio
+    return new ArrayList<>();
+  }
+}
