0b16f39665a0ab3f8066b9132a88ba05302e2a0e
Sys <28715512+sys-001@users.noreply.github.com>
2024-01-17 16:49:59 +0100

    chore: merge changes from dev

--- backend/src/main/java/it/greentrails/backend/gestioneAttivita/repository/AttivitaRepository.java
+++ backend/src/main/java/it/greentrails/backend/gestioneAttivita/repository/AttivitaRepository.java
@@ -1,8 +1,8 @@
 package it.greentrails.backend.gestioneAttivita.repository;
 
 import it.greentrails.backend.entities.Attivita;
-import java.util.Optional;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
