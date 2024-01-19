eefc5343ddf08cc4fd90e34701a6fd4d245bf494
esetaro2 <bbot3333@gmail.com>
2024-01-19 09:48:52 +0100

    feat(frontend): add review feature;

--- frontend/src/app/componenti/pagina-attivita/descrizione-attivita/descrizione-attivita.component.ts
+++ frontend/src/app/componenti/pagina-attivita/descrizione-attivita/descrizione-attivita.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Attivita } from 'src/app/classi/attivita';
-import { AttivitaService } from 'src/app/servizi/attivita.service';
 
 @Component({
   selector: 'app-descrizione-attivita',
