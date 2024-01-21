f5ace5d6cb9a69193bd636a0f372580adb14c473
Valainie <d.cojoc@studenti.unisa.it>
2024-01-21 19:28:23 +0100

    feat(frontend): add style

--- frontend/src/app/componenti/home-page/home-page.component.ts
+++ frontend/src/app/componenti/home-page/home-page.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  nomeAttivita: string = 'nome';
+  immagine_attivita: string = 'https://www.hotelkennedy.org/static/f9f7a02b44b26255144bc1b4086fbd1e/5267c/e825f4c8-e7d7-4693-9895-b012d2879684.jpg';
 
   constructor() { }
 
