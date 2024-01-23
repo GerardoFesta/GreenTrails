adf2549bf2e8410692201145eca6d023a5361157
Ikolfrey <117439704+ikolfrey@users.noreply.github.com>
2024-01-23 08:57:16 +0100

    feat(backend): add availability check before reservation

--- backend/src/main/java/it/greentrails/backend/gestioneprenotazioni/repository/PrenotazioneAlloggioRepository.java
+++ backend/src/main/java/it/greentrails/backend/gestioneprenotazioni/repository/PrenotazioneAlloggioRepository.java
@@ -1,6 +1,7 @@
 package it.greentrails.backend.gestioneprenotazioni.repository;
 
 import it.greentrails.backend.entities.PrenotazioneAlloggio;
+import java.util.Date;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,4 +17,17 @@ public interface PrenotazioneAlloggioRepository extends JpaRepository<Prenotazio
 
   @Query("SELECT p FROM PrenotazioneAlloggio p WHERE p.itinerario.id = ?1")
   Page<PrenotazioneAlloggio> findByItinerario(Long idItinerario, Pageable pageable);
+
+  @Query("""
+SELECT COALESCE(SUM(p.numCamere), 0) FROM Attivita a
+LEFT JOIN Camera c
+LEFT JOIN PrenotazioneAlloggio p
+WHERE a.isAlloggio = TRUE
+AND (
+?1 BETWEEN p.dataInizio AND p.dataFine OR
+?2 BETWEEN p.dataInizio AND p.dataFine OR
+p.dataInizio BETWEEN ?1 AND ?2
+OR p.dataFine BETWEEN ?1 AND ?2)
+""")
+  int getPostiOccupatiTra(Date dataInizio, Date dataFine);
 }
