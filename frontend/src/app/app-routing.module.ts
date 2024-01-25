71fdb87f5455a1650664d1334fa1774dec63ca6c
esetaro2 <bbot3333@gmail.com>
2024-01-26 00:28:11 +0100

    feat(frontend): add ricerca/posizione routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './componenti/login/login.component';
 import { PaginaAttivitaComponent } from './componenti/pagina-attivita/pagina-attivita.component';
 import { RegistrazioneComponent } from './componenti/registrazione/registrazione.component';
 import { GestionePrenotazioniAttiveComponent } from './componenti/gestione-prenotazioni-attive/gestione-prenotazioni-attive.component';
+import { RisultatiComponent } from './componenti/ricerca/risultati/risultati.component';
 
 const routes: Routes = [
   { path: 'registrazione', component: RegistrazioneComponent },
   {path: 'attivita/:id', component: PaginaAttivitaComponent},
   {path: 'prenotazioni', component: GestionePrenotazioniAttiveComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'ricerca/posizione', component: RisultatiComponent}
 
 ];
 
