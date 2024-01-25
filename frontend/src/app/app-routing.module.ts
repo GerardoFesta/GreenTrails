7a389fba918dfea4c25e8c51e117e2412467c14a
esetaro2 <bbot3333@gmail.com>
2024-01-25 20:44:19 +0100

    feat(frontend): add test routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,18 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './componenti/login/login.component';
 import { PaginaAttivitaComponent } from './componenti/pagina-attivita/pagina-attivita.component';
 import { RegistrazioneComponent } from './componenti/registrazione/registrazione.component';
-import { HomePageComponent } from './componenti/home-page/home-page.component';
 import { GestionePrenotazioniAttiveComponent } from './componenti/gestione-prenotazioni-attive/gestione-prenotazioni-attive.component';
 
 const routes: Routes = [
   { path: 'registrazione', component: RegistrazioneComponent },
   {path: 'attivita/:id', component: PaginaAttivitaComponent},
-  {path: '', component: HomePageComponent},
   {path: 'prenotazioni', component: GestionePrenotazioniAttiveComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
 
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
