fe71f2f10039bae3d327601e1f0d65bb5de6e52c
esetaro2 <bbot3333@gmail.com>
2024-01-25 20:53:28 +0100

    fix(frontend): removed mock homepage component from app routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,10 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './componenti/login/login.component';
 import { PaginaAttivitaComponent } from './componenti/pagina-attivita/pagina-attivita.component';
 import { RegistrazioneComponent } from './componenti/registrazione/registrazione.component';
-import { HomepageComponent } from './componenti/homepage/homepage.component';
 
 const routes: Routes = [
-  {path: '', component: HomepageComponent},
   { path: 'login', component: LoginComponent },
   { path: 'registrazione', component: RegistrazioneComponent },
   {path: 'attivita/:id', component: PaginaAttivitaComponent},
