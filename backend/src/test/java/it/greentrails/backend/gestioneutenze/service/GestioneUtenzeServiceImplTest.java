416663f5119530153ec14a16093803ffda65b833
esetaro2 <bbot3333@gmail.com>
2024-02-07 21:56:43 +0100

    fix(backend): fixed checkstyle

--- backend/src/test/java/it/greentrails/backend/gestioneutenze/service/GestioneUtenzeServiceImplTest.java
+++ backend/src/test/java/it/greentrails/backend/gestioneutenze/service/GestioneUtenzeServiceImplTest.java
@@ -8,6 +8,7 @@ import it.greentrails.backend.entities.Preferenze;
 import it.greentrails.backend.gestioneutenze.repository.PreferenzeRepository;
 import it.greentrails.backend.gestioneutenze.repository.UtenteRepository;
 import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
