c57e953cff7d1f60b6891dbc6186d3cdfa0aca52
esetaro2 <bbot3333@gmail.com>
2024-02-07 22:15:48 +0100

    fix(backend): fixed checkstyle

--- backend/src/test/java/it/greentrails/backend/gestioneitinerari/service/ItinerariServiceImplTest.java
+++ backend/src/test/java/it/greentrails/backend/gestioneitinerari/service/ItinerariServiceImplTest.java
@@ -1,9 +1,14 @@
 package it.greentrails.backend.gestioneitinerari.service;
 
+import static org.mockito.Mockito.when;
+
 import it.greentrails.backend.entities.Itinerario;
 import it.greentrails.backend.entities.Utente;
 import it.greentrails.backend.enums.RuoloUtente;
 import it.greentrails.backend.gestioneitinerari.repository.ItinerariRepository;
+
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,11 +18,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-
-import java.util.Collections;
-import java.util.List;
-
-import static org.mockito.Mockito.when;
 
 public class ItinerariServiceImplTest {
 
@@ -34,48 +34,34 @@ public class ItinerariServiceImplTest {
 
   @Test
   void testFindItinerariByUtente() throws Exception {
-    // Mocking utente
     Utente utente = new Utente();
     utente.setId(1L);
     utente.setRuolo(RuoloUtente.VISITATORE);
 
-    // Mocking repository
     when(repository.findByVisitatore(utente.getId(), Pageable.unpaged()))
         .thenReturn(new PageImpl<>(Collections.emptyList()));
 
-    // Call service method
     List<Itinerario> result = itinerariService.findItinerariByUtente(utente);
 
-    // Assertions
     Assertions.assertNotNull(result);
     Assertions.assertTrue(result.isEmpty());
   }
 
   @Test
   void testFindItinerariByNullUtente() {
-
-    // Test per verificare se viene lanciata un'eccezione quando l'utente è null
     Assertions.assertThrows(Exception.class, () -> {
       itinerariService.findItinerariByUtente(null);
     });
-
-
   }
 
   @Test
   void testFindItinerariByNonVisitorUtente() {
-    // Test per verificare se viene lanciata un'eccezione quando l'utente ha un ruolo diverso da VISITATORE
     Utente utenteNonVisitatore = new Utente();
     utenteNonVisitatore.setId(1L);
     utenteNonVisitatore.setRuolo(RuoloUtente.AMMINISTRATORE);
-
-    /*    when(repository.findByVisitatore(utenteNonVisitatore.getId(), Pageable.unpaged()))
-                .thenReturn(new PageImpl<>(Collections.emptyList()));
-*/
 
     Assertions.assertThrows(Exception.class, () -> {
       itinerariService.findItinerariByUtente(utenteNonVisitatore);
     });
   }
 }
-
