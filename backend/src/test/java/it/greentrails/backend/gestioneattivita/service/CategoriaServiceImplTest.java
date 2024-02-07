e25847416d4a6a45811f84338f742b9774bb9ad2
Sys <28715512+sys-001@users.noreply.github.com>
2024-02-07 22:04:13 +0100

    style(backend): refactor test to comply with checkstyle rules

--- backend/src/test/java/it/greentrails/backend/gestioneattivita/service/CategoriaServiceImplTest.java
+++ backend/src/test/java/it/greentrails/backend/gestioneattivita/service/CategoriaServiceImplTest.java
@@ -1,80 +1,89 @@
 package it.greentrails.backend.gestioneattivita.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 import it.greentrails.backend.entities.Categoria;
 import it.greentrails.backend.gestioneattivita.repository.CategoriaRepository;
-import it.greentrails.backend.gestioneattivita.service.CategoriaServiceImpl;
-import java.util.Set;
-
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
-import static org.mockito.ArgumentMatchers.any;
-
 public class CategoriaServiceImplTest {
-    @Mock
-    private CategoriaRepository categoriaRepository;
 
-    @InjectMocks
-    private CategoriaServiceImpl categoriaService;
+  @Mock
+  private CategoriaRepository categoriaRepository;
 
-    private Validator validator;
+  @InjectMocks
+  private CategoriaServiceImpl categoriaService;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        validator = Validation.buildDefaultValidatorFactory().getValidator();
-    }
+  private Validator validator;
 
-    @Test
-    void saveCategoria_Success() throws Exception {
-        Categoria categoria = new Categoria();
-        categoria.setNome("Relax e Benessere");
-        categoria.setDescrizione("Attività e alloggi per il relax e il benessere. Include spa, centri benessere, o alloggi con servizi di spa e benessere.");
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+    validator = Validation.buildDefaultValidatorFactory().getValidator();
+  }
 
-        when(categoriaRepository.save(any())).thenReturn(categoria);
+  @Test
+  void saveCategoriaSuccess() throws Exception {
+    Categoria categoria = new Categoria();
+    categoria.setNome("Relax e Benessere");
+    categoria.setDescrizione(
+        "Attività e alloggi per il relax e il benessere." +
+            " Include spa, centri benessere, o alloggi con servizi di spa e benessere.");
 
-        Categoria savedCategoria = categoriaService.saveCategoria(categoria);
+    when(categoriaRepository.save(any())).thenReturn(categoria);
 
-        assertNotNull(savedCategoria);
+    Categoria savedCategoria = categoriaService.saveCategoria(categoria);
 
-    }
+    assertNotNull(savedCategoria);
 
-    @Test
-    void saveCategoria_NullCategoria_ExceptionThrown() {
-        Categoria nullCategoria = null;
+  }
 
-        assertThrows(Exception.class, () -> categoriaService.saveCategoria(nullCategoria));
+  @Test
+  void saveCategoriaNullCategoriaExceptionThrown() {
+    Categoria nullCategoria = null;
 
-    }
+    assertThrows(Exception.class, () -> categoriaService.saveCategoria(nullCategoria));
 
-    @Test
-    void saveCategoria_NullName_ExceptionThrown() {
-        Categoria categoria = new Categoria();
-        categoria.setNome(null);
-        categoria.setDescrizione("Attività e alloggi per il relax e il benessere. Include spa, centri benessere, o alloggi con servizi di spa e benessere.");
+  }
 
-        Set<ConstraintViolation<Categoria>> violations = validator.validate(categoria);
-        assertFalse(violations.isEmpty());
-        assertEquals("Il nome non può essere vuoto.", violations.iterator().next().getMessage());
-    }
+  @Test
+  void saveCategoriaNullNameExceptionThrown() {
+    Categoria categoria = new Categoria();
+    categoria.setNome(null);
+    categoria.setDescrizione(
+        "Attività e alloggi per il relax e il benessere." +
+            " Include spa, centri benessere, o alloggi con servizi di spa e benessere."
+    );
 
-    @Test
-    void saveCategoria_NullDescription_ExceptionThrown() {
-        Categoria categoria = new Categoria();
-        categoria.setNome("Relax e Benessere");
-        categoria.setDescrizione(null);
+    Set<ConstraintViolation<Categoria>> violations = validator.validate(categoria);
+    assertFalse(violations.isEmpty());
+    assertEquals("Il nome non può essere vuoto.",
+        violations.iterator().next().getMessage());
+  }
 
-        Set<ConstraintViolation<Categoria>> violations = validator.validate(categoria);
-        assertFalse(violations.isEmpty());
-        assertEquals("La descrizione non può essere vuota.", violations.iterator().next().getMessage());
-    }
+  @Test
+  void saveCategoriaNullDescriptionExceptionThrown() {
+    Categoria categoria = new Categoria();
+    categoria.setNome("Relax e Benessere");
+    categoria.setDescrizione(null);
+
+    Set<ConstraintViolation<Categoria>> violations = validator.validate(categoria);
+    assertFalse(violations.isEmpty());
+    assertEquals("La descrizione non può essere vuota.",
+        violations.iterator().next().getMessage());
+  }
 
 }
